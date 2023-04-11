47c70699275c2624fc3a85a855fcdd43b3abf187
Owen Davies <oaster2000@gmail.com>
2023-04-11 02:41:06 +0100

    feat: assets

--- server_app/application.py
+++ server_app/application.py
@@ -38,4 +38,4 @@ def place():
     return jsonify(app.tweet_data.place)
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0" debug=True, use_reloader=False)
+        app.run(host="0.0.0.0", debug=True, use_reloader=False)
